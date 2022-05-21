7ed5c8406083ab81c5eec99106299921c4159654
Richard Caleb Lawver <clawver30@gmail.com>
2022-05-21 11:02:47 -0600

    Employee tests done

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,58 @@
 const Employee = require('../lib/Employee');
 
-test('creates employee object', () => {
-    const employee = new Employee('Ryan', 555, 'ryanO@gmail.com');
+describe('Employee', () => {
+    test('Creating employee object', () => {
+        const emp = new Employee();
+        expect(typeof(emp)).toBe('object');
+    });
 
-    expect(employee.name).toBe('Ryan');
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toBe('ryanO@gmail.com')
-});
+    test('Can create a name', () => {
+        const name = 'Caleb';
+        const emp = new Employee(name);
+        expect(emp.name).toBe(name);
+    });
+
+    test('Can get ID', () => {
+        const fakeId = 10101010;
+        const emp = new Employee('Caleb', fakeId);
+        expect(emp.id).toBe(fakeId);
+    });
+
+    test('Setting email address', () => {
+        const fakeEmail = 'tests@tests.com'
+        const emp = new Employee('Caleb', 5, fakeEmail);
+        expect(emp.email).toBe(fakeEmail);
+    });
+
+    describe('getName', () => {
+        test('Can get name from getName', () => {
+            const name = 'Caleb';
+            const emp = new Employee(name);
+            expect(emp.getName()).toBe(name);
+        });
+    });
+
+    describe('getId', () => {
+        test('Can get ID from getId', () => {
+            const fakeId = 10101010;
+            const emp = new Employee('Caleb', fakeId);
+            expect(emp.getId()).toBe(fakeId);
+        });
+    });
+
+    describe('getEmail', () => {
+        test('Can getEmail', () => {
+            const fakeEmail = 'tests@tests.com'
+            const emp = new Employee('Caleb', 5, fakeEmail);
+            expect(emp.getEmail()).toBe(fakeEmail);
+        });
+    });
+
+    describe('getsRole', () => {
+        test('getting the role', () => {
+            const fakeRole = 'Employee'
+            const emp = new Employee('Caleb', 5, 'test@test.com');
+            expect(emp.getRole()).toBe(fakeRole);
+        });
+    });
+})
