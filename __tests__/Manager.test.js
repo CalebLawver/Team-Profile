b9f4abc9729052272df37cda13be359f80aa6ddb
Richard Caleb Lawver <clawver30@gmail.com>
2022-05-21 11:14:45 -0600

    Manager test added

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,22 @@
+const Manager = require('../lib/Manager');
+
+test('Can get officeNumber', () => {
+    const fakeOffice = 10;
+    const man = new Manager('Ryan', 1, 'test@tests.com', fakeOffice);
+    expect(man.officeNumber).toBe(fakeOffice);
+});
+
+test('getOfficeNumber returns number', () => {
+    const fakeOffice = 10;
+    const man = new Manager('Caleb', 5, 'test@test.com', fakeOffice);
+    expect(man.getOfficeNumber()).toBe(fakeOffice);
+})
+
+test('getRole() returning manager', () => {
+    const fakeRole = 'Manager';
+    const man = new Manager('Caleb', 5, 'test@test.com', 10);
+    expect(man.getRole()).toBe(fakeRole);
+});
 
 
 
