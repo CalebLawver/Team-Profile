0bcc333631d0225d2babeb05d2211b053b8b2f7f
Richard Caleb Lawver <clawver30@gmail.com>
2022-06-19 20:19:18 -0600

    fixed issue with .then

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const employees = [// storing the objects created by the prompts
 
 function startIndex() {
     addEmployee();
-    // make HTML();
+    beginHtml();
 }
 
 function addEmployee() {
@@ -30,7 +30,7 @@ function addEmployee() {
             type: 'list',
             message: "Enter the team member's role",
             name: 'role',
-            choices: ['Manager', 'Engineer', 'Intern']
+            choices: ['Manager', 'Engineer', 'Intern', ]
         }
     ])
     .then(function({name, id, email, role}) {
@@ -59,16 +59,17 @@ function createManager(name, id, email) {
             name: 'moreEmployee'
         }
     ])
-    .then(function({officeNumber, moreEmployees}) {
+    .then(function({officeNumber, moreEmployee}) {
         const newManager = new Manager(name, id, email, officeNumber);
+        employees.push(newManager);
         addHtml(newManager)
-        .then(function() {
-           if(moreEmployees === 'Yes') {
-                return addEmployee;
+        
+           if(moreEmployee === 'Yes') {
+                addEmployee();
             } else {
                 finishHtml();
             } 
-        })
+        
         
     })
 }
@@ -82,20 +83,21 @@ function createEngineer(name, id, email) {
         {
             type: 'list',
             message: "Would you like to add another Employee?",
-            choices: ['Yes', 'No'],
+            choices: ['yes', 'No'],
             name: 'moreEmployee'
         }
     ])
-    .then(function({github, moreEmployees}) {
+    .then(function({github, moreEmployee}) {
         const newEngineer = new Engineer(name, id, email, github);
+        employees.push(newEngineer);
         addHtml(newEngineer)
-        .then(function() {
-            if(moreEmployees === 'Yes') {
-                 return addEmployee;
+        
+            if(moreEmployee === 'Yes') {
+                addEmployee();
              } else {
-                 finishHtml();
+                finishHtml();
              } 
-         })
+        
     })
 }
 
@@ -112,17 +114,105 @@ function createIntern(name, id, email) {
             name: 'moreEmployee'
         }
     ])
-    .then(function({school, moreEmployees}) {
+    .then(function({school, moreEmployee}) {
         const newIntern = new Intern(name, id, email, school);
+        employees.push(newIntern);
         addHtml(newIntern)
-        .then(function() {
-            if(moreEmployees === 'Yes') {
-                 return addEmployee;
+        
+            if(moreEmployee === 'Yes') {
+                addEmployee();
              } else {
-                 finishHtml();
+                finishHtml();
              } 
-         })
+        
     })
 }
+
+function beginHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+    fs.writeFile('./dist/team.html', html, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+function addHtml(employee) {
+    const name = employee.getName();
+    const role = employee.getRole();
+    const id = employee.getId();
+    const email = employee.getEmail();
+
+    let html = '';
+
+    if (role === 'Engineer') {
+        const github = employee.getGithub();
+        html = `<div class="col-6">
+        <div class="card mx-auto mb-3" style="width: 18rem">
+        <h5 class="card-header">${name}<br /><br />Engineer</h5>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${id}</li>
+            <li class="list-group-item">Email Address: ${email}</li>
+            <li class="list-group-item">GitHub: ${github}</li>
+        </ul>
+        </div>
+    </div>`;
+    } else if (role === 'Manager') {
+        const officeNumber = employee.getOfficeNumber();
+        html = `<div class="col-6">
+        <div class="card mx-auto mb-3" style="width: 18rem">
+        <h5 class="card-header">${name}<br /><br />Manager</h5>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${id}</li>
+            <li class="list-group-item">Email Address: ${email}</li>
+            <li class="list-group-item">GitHub: ${officeNumber}</li>
+        </ul>
+        </div>
+    </div>`;
+    } else {
+        const school = employee.getSchool();
+        html = `<div class="col-6">
+        <div class="card mx-auto mb-3" style="width: 18rem">
+        <h5 class="card-header">${name}<br /><br />Intern</h5>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${id}</li>
+            <li class="list-group-item">Email Address: ${email}</li>
+            <li class="list-group-item">GitHub: ${school}</li>
+        </ul>
+        </div>
+    </div>`
+    }
+
+    fs.appendFile('./dist/team.html', html, function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    })
+}
+
+function finishHtml() {
+    console.log('File has been created');
+    const html = `</div>
+    </div>
+</body>
+</html>`
+}
+
+// need to add beginning HTML, then add a function to add each member. 
 
 startIndex();
